6413c1b5ebadeb82d0dd402595cd2bd30e31fb4f
Эдгар Томский <EdgarKozlov3229@gmail.com>
2024-11-07 20:54:48 +0300

    Пофикшен тайтлбар, а точнее нумерация версий и вытекающая проблема с неправильным размещением элементов.

--- src/titlebar.rs
+++ src/titlebar.rs
@@ -1,6 +1,8 @@
 use leptos::*;
 use wasm_bindgen::prelude::*;
 
+const VERSION: &str = "v2.0.0-beta.1";
+
 #[wasm_bindgen(module = "/src/window.js")]
 extern "C" {
     #[wasm_bindgen]
@@ -17,11 +19,11 @@ extern "C" {
 pub fn Titlebar() -> impl IntoView {
     view! {
         <div data-tauri-drag-region class="flex-element flex-row w-full justify-between">
-            <div data-tauri-drag-region class="flex-element flex-row">
+            <div data-tauri-drag-region class="flex-element flex-row w-full">
                 <span data-tauri-drag-region class="panel flex-element font-bold">"pts_bomber"</span>
-                <span class="panel flex-element font-light text"><em>"v2.0.0"</em></span>
+                <span class="panel flex-element w-full font-light"><em>{VERSION}</em></span>
+                <span data-tauri-drag-region class="panel flex-element w-full font-bold">"Is idle"</span>
             </div>
-            <span data-tauri-drag-region class="panel w-full font-bold">"Is idle"</span>
             <div class="panel material-symbols-rounded flex-element flex-row">
                 <button on:click=move |_| { minimize() }>"minimize"</button>
                 <button on:click=move |_| { close() }>"close"</button>
