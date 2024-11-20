0859ae2278c529ca24384651f5ded09e4f93acc9
Эдгар Томский <EdgarKozlov3229@gmail.com>
2024-11-20 20:42:14 +0300

    Дополнил О программе.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -61,7 +61,7 @@ async fn attack(phone: String) {
 async fn show_about_window(app: tauri::AppHandle) {
     tauri::WebviewWindowBuilder::new(&app, "about", tauri::WebviewUrl::App("/about".into()))
         .title("О программе")
-        .inner_size(400.0, 350.0)
+        .inner_size(350.0, 600.0)
         .resizable(false)
         .build()
         .unwrap();
--- src/about/mod.rs
+++ src/about/mod.rs
@@ -3,8 +3,18 @@ use leptos::*;
 #[component]
 pub fn About() -> impl IntoView {
     view! {
-        <div class="panel flex-element flex-auto justify-around"><img src="../public/icon.svg" alt="Icon" class="w-24 h-24"/></div>
-        <div class="panel flex-element flex-auto justify-around bg-red-700 font-bold">Данные проект несёт исключительно образовательней цели и вообще создаётся как портфолио.</div>
-        <div class="panel flex-element flex-auto justify-around">Первый SMS/звонки бомбер написанный на Rust</div>
+        <div class="panel flex-element"><img src="../public/icon.svg" alt="Icon" class="w-24 h-24"/></div>
+        <div class="panel flex-element justify-around bg-red-700 font-bold">Данные проект несёт исключительно образовательней цели и вообще создаётся как портфолио.</div>
+        <div class="panel flex-element justify-around">Первый SMS/звонки бомбер написанный на Rust</div>
+        <div class="panel flex-element justify-around">Используемые технологии</div>
+        <ul class="panel flex-element flex-auto justify-around">
+            <li>Rust</li>
+            <li>Tauri</li>
+            <li>Leptos</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>GitHub Actions</li>
+            <li>Arch Linux</li>
+        </ul>
     }
 }
