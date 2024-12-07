ea0b8fe3367d059aaa2c95e4e23773d55a4b773b
Эдгар Томский <EdgarKozlov3229@gmail.com>
2024-12-07 23:49:34 +0300

    Максимально оптимизированный подвал

--- src/footer/mod.rs
+++ src/footer/mod.rs
@@ -15,13 +15,13 @@ extern "C" {
 pub fn Footer() -> impl IntoView {
     view! {
         <div class="flex-element flex-row w-full justify-around">
-            <a href="https://github.com/Patysonchick/pts_bomber" class="panel flex-element rounded-b-none" target="_blank">
+            <a href="https://github.com/Patysonchick/pts_bomber" class="panel rounded-b-none" target="_blank">
                 <img src="../public/github-mark.svg" alt="GitHub" class="h-7" />
             </a>
-            <a href="https://t.me/pts_bomber" class="panel flex-element rounded-b-none" target="_blank">
+            <a href="https://t.me/pts_bomber" class="panel rounded-b-none" target="_blank">
                 <img src="../public/telegram.svg" alt="Telegram" class="h-7" />
             </a>
-            <button class="panel flex-element rounded-b-none" on:click=move |_| {
+            <button class="panel rounded-b-none" on:click=move |_| {
                 spawn_local(async move {
                     invoke_without_args("show_about_window").await;
                 });
