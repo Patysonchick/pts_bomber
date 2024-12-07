7b50b1c166b4dff3ecbcde8a943ce62f3c2de2a3
Эдгар Томский <EdgarKozlov3229@gmail.com>
2024-12-07 23:47:46 +0300

    Максимально оптимизированный тайтлбар

--- src/titlebar/mod.rs
+++ src/titlebar/mod.rs
@@ -25,22 +25,20 @@ pub fn Titlebar(title: ReadSignal<Status>) -> impl IntoView {
 
     view! {
         <div data-tauri-drag-region class="flex-element flex-row w-full justify-between">
-            <div data-tauri-drag-region class="flex-element flex-row w-full">
-                <span data-tauri-drag-region class="panel flex-element font-bold">"pts_bomber"</span>
-                <span class="panel flex-element w-full font-light"><em>{version}</em></span>
-                <span
-                    data-tauri-drag-region
-                    class="panel flex-element w-full font-bold"
-                    class: bg-neutral-800 = move || title.get() == Status::IsIdling
-                    class: bg-red-700 = move || title.get() == Status::Attacking
-                >{
-                    move || match title.get() {
-                        Status::IsIdling => "Простаивает",
-                        Status::Attacking => "Атака"
-                    }
-                }</span>
-            </div>
-            <div class="panel material-symbols-rounded flex-element flex-row">
+            <span data-tauri-drag-region class="panel font-bold">"pts_bomber"</span>
+            <em class="panel font-light whitespace-nowrap">{version}</em>
+            <span
+                data-tauri-drag-region
+                class="panel w-full font-bold"
+                class: bg-neutral-800 = move || title.get() == Status::IsIdling
+                class: bg-red-700 = move || title.get() == Status::Attacking
+            >{
+                move || match title.get() {
+                    Status::IsIdling => "Простаивает",
+                    Status::Attacking => "Атака"
+                }
+            }</span>
+            <div class="panel material-symbols-rounded">
                 <button on:click=move |_| { minimize() }>"minimize"</button>
                 <button on:click=move |_| { close() }>"close"</button>
             </div>
